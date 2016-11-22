ca60d82a7703889821b763137a65fc028d4f162c
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 22:15:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-  console.log( server.address().address );
-})
+});
+
+console.log( server.address().address );
