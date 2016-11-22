4734134d9742c93e912e7db516bcc4cbae5ff692
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 22:35:29 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-
-  var server = net.createServer((socket) => {
-    socket.end('goodbye\n');
-  }).on('error', (err) => {
-  // handle errors here
-    throw err;
-  });
-
-  // grab a random port.
-  server.listen(() => {
-    console.log('opened server on', server.address());
-  });
-  
+  console.log('Example app listening on port 3000!');  
 });
 
   
